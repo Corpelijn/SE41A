132f1219aac7ed293237c0c95ce8eb9ce8afb5dc
Sjors <spm.vanmierlo@student.fontys.nl>
2015-10-07 13:36:19 +0200

    Inloggen

--- FireBee/FireBee/Form1.cs
+++ FireBee/FireBee/Form1.cs
@@ -9,11 +9,16 @@ using System.Windows.Forms;
 
 namespace FireBee
 {
-    public partial class Form1 : Form
+    public partial class inloggen : Form
     {
-        public Form1()
+        public inloggen()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
